5465cc38c66dc5b319c7a531fd56c63e16aa9790
harshinibaskar <51949520+harshinibaskar@users.noreply.github.com>
2019-06-28 15:04:31 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,7 @@
+s=input()
+if(s=='a','e','i','o','u'):
+  print("Vowel")
+elif(s=='b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("Consonant")
+else:
+  print("Invalid")
